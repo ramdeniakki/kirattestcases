8cbaa12207eaab3780f9c4a136edae09af90fdea
Ramdeni Akshith <117385626+ramdeniakki@users.noreply.github.com>
2024-10-27 20:35:37 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -13,7 +13,7 @@ const value = {
 const token =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWx1ZSI6eyJuYW1lIjoiYWtraSIsImFjY291bnROdW1iZXIiOiI0NTY5NzE2NTQ2NTMiLCJhY2NvdW50QnJhbmNoIjoiU0JJIn0sImlhdCI6MTcyOTY2MDEyNn0.PfeKJevIk8uBZ8kSx5_TJ8X10h0cH-ACiOqUolaS9YQ";
 
-const secretKey = "JOKER";
+const secretKey = "SECERET";
 
 try {
 	const verified = jwt.verify(token, secretKey);
